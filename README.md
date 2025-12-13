01db63441753ff8312f56b88fa89fdff451e0d03
Machinacanis <hf4minecraft@163.com>
2025-12-13 20:24:10 +0800

    初始化docusaurus文档仓库

---
+++ README.md
@@ -0,0 +1,7 @@
+# Togawa Dev Documents
+
+Sakiko 及其周边生态项目的文档仓库。
+
+使用 Docusarus 搭建，详见 https://togawa-dev.github.io
+
+还在施工状态，再等等哦 🚧
---
+++ docusaurus.config.ts
@@ -0,0 +1,162 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: "SAKIKO",
+  tagline: "A cross-platform, scalable chatbot framework",
+  favicon: "img/favicon.ico",
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: "https://togawa-dev.github.io",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "togawa-dev", // Usually your GitHub org/user name.
+  projectName: "@togawa-dev/sakiko", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "cn",
+    locales: ["cn"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: "img/docusaurus-social-card.jpg",
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+    navbar: {
+      title: "Togawa Dev",
+      logo: {
+        alt: "My Site Logo",
+        src: "img/logo.svg",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "sakiko",
+          label: "Sakiko",
+          position: "left",
+        },
+        {
+          type: "docSidebar",
+          sidebarId: "progressive",
+          position: "left",
+          label: "渐进式指南",
+        },
+        {
+          type: "dropdown",
+          label: "Sakiko 生态项目",
+          position: "right",
+          items: [
+            {
+              type: "docSidebar",
+              sidebarId: "uika",
+              label: "Uika",
+            },
+            {
+              type: "docSidebar",
+              sidebarId: "umiri",
+              label: "Umiri",
+            },
+            {
+              type: "docSidebar",
+              sidebarId: "mutsumi",
+              label: "Mutsumi",
+            },
+          ],
+        },
+        {
+          href: "https://github.com/togawa-dev/sakiko",
+          label: "Github",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Tutorial",
+              to: "/docs/intro",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/docusaurus",
+            },
+            {
+              label: "Discord",
+              href: "https://discordapp.com/invite/docusaurus",
+            },
+            {
+              label: "X",
+              href: "https://x.com/docusaurus",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Blog",
+              to: "/blog",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/facebook/docusaurus",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} The Sakiko Project. Built with Docusaurus & ♥️.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
